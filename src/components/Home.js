f03cfeca1bbdcd63e7cfe696b9266f4863a01754
DSP Chathuranga <dspchathuranga@gmail.com>
2023-10-11 12:48:06 +0100

    vehicle and equipments view done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useGetAllVehiclesQuery } from "../api/vehicleSlice";
 import { useGetAllEquipmentsQuery } from "../api/equipmentSlice";
+import VehicleCard from "./vehicle/VehicleCard";
 
 const Home = () => {
-  const {data:vehicles} = useGetAllVehiclesQuery();
-    console.log(vehicles);
+  const { data: vehicles } = useGetAllVehiclesQuery();
+  console.log(vehicles);
 
-    const {data:equipments, error, isLoading} = useGetAllEquipmentsQuery();
-    console.log(equipments);
+  const { data: equipments, error, isLoading } = useGetAllEquipmentsQuery();
+  console.log(equipments);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Home
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        {vehicles ? vehicles.map((vehicle, idx) => {
+          return <VehicleCard data={{vehicle:vehicle,equipments}} key={idx} />;
+        }) : ""}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
