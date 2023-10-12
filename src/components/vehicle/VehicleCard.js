37f832e69d557a1abb5ea2fed1ad203c5d1afe9a
DSP Chathuranga <dspchathuranga@gmail.com>
2023-10-12 09:07:45 +0100

    change UI

--- src/components/vehicle/VehicleCard.js
+++ src/components/vehicle/VehicleCard.js
@@ -1,50 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import deleteIcon from "../../assets/deleteIcon.svg";
+import PopupModal from "../common/PopupModal";
 
-const VehicleCard = ({ data }) => {
+const VehicleCard = ({ data, handler }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="col">
-      <div className="card h-100">
+    <div className="col-12 col-md-4">
+      <div className="card h-100 shadow p-2 mb-1 border-0 rounded">
         <div className="card-body">
-          <h5 className="card-title">{data.vehicle.id}</h5>
-          <p className="card-text">Name: {data.vehicle.name}</p>
+          <h5 className="card-title d-none">{data.vehicle.id}</h5>
+          <h5 className="card-text d-flex justify-content-between align-items-center">
+            <span>Name: {data.vehicle.name}</span>
+            <button
+              className="btn btn-warning"
+              onClick={toggleModal}
+            >
+              Edit
+            </button>
+          </h5>
           <p className="card-text">Driver: {data.vehicle.driver}</p>
           <p className="card-text">Status: {data.vehicle.status}</p>
           <p className="card-text">Fuel Type: {data.vehicle.fuelType}</p>
           <h6 className="card-title">Equipments</h6>
-          {data.vehicle.equipments
-            ? data.vehicle.equipments.map((equipmentId) => {
-                console.log("equipmentId : ", data.equipments);
+          <div className="row g-2">
+            {data.vehicle.equipments
+              ? data.vehicle.equipments.map((equipmentId) => {
+                  console.log("equipmentId : ", data.equipments);
 
-                let equipment = data.equipments.find(
-                  (a) => a.id === equipmentId
-                );
-                //console.log("equipment : ", equipment);
-                return (
-                  <>
-                    <div className="row row-cols-1 row-cols-md-2 g-4">
-                      <div className="col">
-                        <div className="card h-100">
-                          <div className="card-body">
-                            <h5 className="card-title">
+                  let equipment = data.equipments.find(
+                    (a) => a.id === equipmentId
+                  );
+                  //console.log("equipment : ", equipment);
+                  return (
+                    <>
+                      <div className="col-auto">
+                        <div className="card h-100 shadow-sm bg-light">
+                          <div className="card-body px-2 py-1">
+                            <h6 className="card-title p-1 m-0">
                               {equipment.id} - {equipment.name}
-                            </h5>
+                              <button className="btn btn-danger btn-small p-1 ms-2">
+                                <img src={deleteIcon} />
+                              </button>
+                            </h6>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </>
-                );
-
-                // return data.equipments.map((equipment) => {
-                //   console.log("equipment : ", equipment);
-                //   return (
-                //     <><p className="card-text">ss</p><br/><p className="card-text">ssss</p></>
-                //   );
-                // });
-              })
-            : ""}
+                    </>
+                  );
+                })
+              : ""}
+          </div>
         </div>
       </div>
+      <PopupModal
+            isOpen={isModalOpen}
+            toggle={toggleModal}
+            title={"Edit Vehicle"}
+            option={"EDITV"}
+            handler={handler}
+          />
     </div>
   );
 };
