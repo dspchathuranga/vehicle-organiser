b0099ebcf6bbe5aa53e5b585782cb88d231649d0
DSP Chathuranga <dspchathuranga@gmail.com>
2023-10-15 15:32:29 +0100

    Tost add

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -64,6 +64,7 @@ function Login() {
 
   return (
     <div className="container mt-5">
+      <ToastContainer />
     <div className="row justify-content-center">
       <div className="col-md-6">
         <div className="card">
